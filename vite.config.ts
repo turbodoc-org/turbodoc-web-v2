0ef91dad849ef45714983a446bd857f45d2e4e9c
Nico <nicolaasb@pm.me>
2026-04-23 20:49:01 +0200

    Set up pipelines

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite-plus'
 import { tanstackStart } from '@tanstack/react-start/plugin/vite'
 import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
