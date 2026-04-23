83d3d5612148597c1dcdf63e44ea41770e2e8ff9
Nico <nicolaasb@pm.me>
2026-04-23 22:38:42 +0200

    Update router

--- src/router.tsx
+++ src/router.tsx
@@ -1,13 +1,20 @@
-import { createRouter } from '@tanstack/react-router';
+import { createRouter } from "@tanstack/react-router";
 
 // Import the generated route tree
-import { routeTree } from './routeTree.gen';
+import { routeTree } from "./routeTree.gen";
 
 // Create a new router instance
 export const getRouter = () => {
   return createRouter({
     routeTree,
     scrollRestoration: true,
+    defaultPreload: "intent",
     defaultPreloadStaleTime: 0,
   });
 };
+
+declare module "@tanstack/react-router" {
+  interface Register {
+    router: ReturnType<typeof getRouter>;
+  }
+}
